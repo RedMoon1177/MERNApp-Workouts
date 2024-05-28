f79c53dab930ec5d70695601875d0243915f612e
Anne Nguyen <baitoanngoisaodo@gmail.com>
2024-05-28 10:52:15 -0300

    commit for useLogout

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout';
 
 export const Navbar = () => {
+
+    const { logout } = useLogout()
+
+    const handleClick = () => {
+        logout()
+    }
+
     return (
         <header>
             <div className='container'>
@@ -11,6 +19,9 @@ export const Navbar = () => {
                     </h1>
                 </Link>
                 <nav>
+                    <div>
+                        <button onClick={handleClick}>Log out</button>
+                    </div>
                     <div>
                         <Link to="/login">Login</Link>
                         <Link to="/signup">Signup</Link>
